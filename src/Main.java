e209663062cc2c7e6a1d699dadd766671cfdcd26
mmmichael-carvalho <162504810+mmmichael-carvalho@users.noreply.github.com>
2026-06-04 23:09:18 -0300

    upd

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("XADREZ");
+
+        Tabuleiro tabuleiro = new Tabuleiro();
+
+        System.out.println(Arrays.deepToString(tabuleiro));
 
     }
 }
