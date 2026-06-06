a26dc50259962e8f7250565bbd4cf4acab35c852
mmmichael-carvalho <162504810+mmmichael-carvalho@users.noreply.github.com>
2026-06-05 22:56:40 -0300

    upd

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("XADREZ");
+        System.out.println("XADREZ.com");
 
         Tabuleiro tabuleiro = new Tabuleiro();
 
